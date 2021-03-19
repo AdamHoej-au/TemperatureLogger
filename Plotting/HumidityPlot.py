955853ca900756fd8867e4cb524f2b9dce72a7be
Adam Høj <au617583@post.au.dk>
2021-03-19 14:53:56 +0100

    Plot test

--- Plotting/HumidityPlot.py
+++ Plotting/HumidityPlot.py
@@ -5,12 +5,15 @@ import matplotlib.cbook as cbook
 import numpy as np
 import pandas as pd
 #%%
+fileInput = "../Logs/humidityLog.csv"
+
 header_list = ["Dato", "Tid", "Temperatur1", "Luftfugtighed1"]
-df = pd.read_csv("humidityLog.csv", names=header_list)
+df = pd.read_csv(fileInput, names=header_list)
 #%%
 
 print(df)
 # # %%
 
 df.plot("Dato", ["Temperatur1", "Luftfugtighed1"], subplots=True)
+plt.savefig('Plot.png',dpi=300)
 # %%
