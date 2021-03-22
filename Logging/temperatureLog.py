4520ad7c65f5817ecaa7e19d854ade889fed61e0
AdamHoej-au <au617583@post.au.dk>
2021-03-22 20:39:32 +0100

    TemperatureLoggerUpdated

--- Logging/temperatureLog.py
+++ Logging/temperatureLog.py
@@ -1,14 +1,33 @@
+#!/usr/bin/env python
+# Found at https://www.raspberrypi.org/forums/viewtopic.php?t=128776
+import glob
 import time
 
-sensors = {"28-00000846c002": 0, "28-0000084718f2": 0}
+# DS18B20.py
+# 2016-04-25
+# Public Domain
 
-while 1:
-    for sensor in sensors:
-        tempfile = open("/sys/bus/w1/devices/{0}/w1_slave".format(sensor))
-        temptext = tempfile.read()
-        tempfile.close()
-        tempcelsius = temptext.split("\n")[1].split(" ")[9]
-        temperature = float(tempcelsius[2:])
-        temperature = temperature / 1000
-        sensors[sensor] = temperature
-    print(' ').join(['{0:2.4f},'.format(v) for k, v in sensors.iteritems()])
+# Typical reading
+# 73 01 4b 46 7f ff 0d 10 41 : crc=41 YES
+# 73 01 4b 46 7f ff 0d 10 41 t=23187
+
+while True:
+
+   for sensor in glob.glob("/sys/bus/w1/devices/28-00*/w1_slave"):
+      id = sensor.split("/")[5]
+
+      try:
+         f = open(sensor, "r")
+         data = f.read()
+         f.close()
+         if "YES" in data:
+            (discard, sep, reading) = data.partition(' t=')
+            t = float(reading) / 1000.0
+            print("{} {:.1f}".format(id, t))
+         else:
+            print("999.9")
+
+      except:
+         pass
+
+   time.sleep(3.0)
