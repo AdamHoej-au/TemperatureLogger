9d191ef8fd02613657a3d50ddaa6a369841cbf7a
AdamHoej-au <au617583@post.au.dk>
2021-03-22 20:54:39 +0100

    Og navne fjernet igen,...

--- Logging/temperatureLog.py
+++ Logging/temperatureLog.py
@@ -10,12 +10,10 @@ import time
 # Typical reading
 # 73 01 4b 46 7f ff 0d 10 41 : crc=41 YES
 # 73 01 4b 46 7f ff 0d 10 41 t=23187
-
 while True:
 
    for sensor in glob.glob("/sys/bus/w1/devices/28-00*/w1_slave"):
-    #   id = sensor.split("/")[5]
-      id = {"Johnny","Allan","Lars"}  
+      id = sensor.split("/")[5]
       try:
          f = open(sensor, "r")
          data = f.read()
