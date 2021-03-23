45a1b6a61df915469c71666a233f7b17df7b11fa
AdamHoej-au <au617583@post.au.dk>
2021-03-23 07:34:30 +0100

    New code...

--- Logging/temperatureLog.py
+++ Logging/temperatureLog.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 # Found at https://www.raspberrypi.org/forums/viewtopic.php?t=128776
+# https://www.raspberrypi.org/forums/viewtopic.php?t=150797
 import glob
 import os
+import glob
+import datetime
 import time
+from time import sleep
+
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
 # DS18B20.py
 # 2016-04-25
@@ -11,57 +18,49 @@ import time
 # Typical reading
 # 73 01 4b 46 7f ff 0d 10 41 : crc=41 YES
 # 73 01 4b 46 7f ff 0d 10 41 t=23187
-# sensors = glob.glob("/sys/bus/w1/devices/28-00*/w1_slave")
-sensors = {"28-00000846c002":"Johnny", "28-0000084718f2":"Allan"}
-logPath = {"/Logfiles/"}
+sensors = {}
+sensors["28-00000846c002"] = "Johnny"
+sensors["28-0000084718f2"] = "Allan"
+# sensors["28-0000084718f2"] = "Lars"
+logFile = {"/Logfiles/TemperatureLog.csv"}
 
 print(sensors)
-    
+
 while True:
-   for sensor in sensors:
-    # for idx, val in enumerate(sensornames):
-    #    print(idx,val)
-    # id = sensor.split("/")[5]
-    id = sensors.get(sensor)
-    try:
-        f = open(sensor, "r")
-        data = f.read()
+   date_log = str(datetime.datetime.now())
+#    print("Next Temp Set")
+   def get_temp(device):
+      #To read the sensor data, just open the w1_slave file
+        f = open(device, 'r')
+        data = f.readlines()
         f.close()
-        if "YES" in data:
-            (discard, sep, reading) = data.partition(' t=')
-            t = float(reading) / 1000.0
-            print("{},{},{},{:.1f}".format(time.strftime('%y/%m/%d'),time.strftime('%H:%M:%S'),id, t))
-        else:
-            print("999.9")
+        deg_f = ''
+        if data[0].strip()[-3:] == 'YES':
+            temp = data[1][data[1].find('t=')+2:]
+              #If temp is 0 or not numeric an exception
+              #will occur so lets handle it gracefully
+            try:
+                if float(temp) == 0:
+                    deg_f = 32
+                else:
+                    deg_f = (float(temp)/1000.00)
+            except:
+                print("Error with t=", temp)
+                pass
+        return deg_f
 
-    except:
-        pass
-
-    time.sleep(3.0)
-
-
-
-
-# import time
-# import csv
-# import Adafruit_DHT
-
-# DHT_SENSOR = Adafruit_DHT.DHT11
-# DHT_PIN = 4
-# try: 
-# 	f = open('/home/pi/humidityLog.csv','a+')
-# 	if os.stat('home/pi/humidityLog.csv').st_size==0:
-# 		f.write('Date,Time,Temperature,Humidity\r\n')
-# 		f.flush()
-# except:
-# 	pass
-
-# while True:
-# 	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR,DHT_PIN)
-# 	if humidity is not None and temperature is not None:
-# 		f.write('{0},{1},{2:0.1f},{3:0.1f}\r\n'.format(time.strftime('%y/%m/%d'),time.strftime('%H:%M:%S'), temperature,humidity))
-# 		f.flush()
-# 	else:
-# 		print("Nej.")
-
-# 	time.sleep(30)
+   for sensor in sensors:
+        sensor_name = sensors[sensor]  # <-- grabs the sensor name given to it
+        with open(logFile, 'a') as f:
+            s = sensor_name + ','
+            s += date_log + ','
+            # <-- path added here to access sensor
+            s += str(get_temp('/sys/bus/w1/devices/' +
+                sensor + '/w1_slave')) + '\r\n'
+            print(s)
+            f.write(s)
+            f.flush()
+          #When there are multiple devices, a short pause
+          #interval between reading sensors seems to work best
+        time.sleep(1)
+   time.sleep(6)
