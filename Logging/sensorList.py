fb0ad9ff0415d40b03a2077801f1db7960ca9557
AdamHoej-au <au617583@post.au.dk>
2021-04-07 13:32:43 +0200

    sensornavne

--- Logging/sensorList.py
+++ Logging/sensorList.py
@@ -1,5 +1,5 @@
 sensors = {
-    "28-021792453713": "Johnny Madsen",
-    "28-021892456ecb": "Allan Olsen",
-    "28-02119245e86c": "Lars Lilholt"
-}
+  "28-021792453713": "Johnny Madsen",   
+ "28-021892456ecb": "Allan Olsen", 
+ "28-02119245e86c": "Lars Lilholt"
+ }
